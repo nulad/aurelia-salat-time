9c7ee3a83bd77f9349a03c491961ddcb6f70ce81
Nulad Wisnu Pambudi <nulad.pambudi@erraipasifik.com>
2018-02-23 00:34:45 +0700

    updated the salatconfig to use aladhan https endpoint

--- src/config/salatConfig.js
+++ src/config/salatConfig.js
@@ -1,6 +1,6 @@
 const salatConfig = {};
 
 salatConfig.locationApiUrl = 'https://nominatim.openstreetmap.org/reverse?format=json&zoom=15&addressdetails=1';
-salatConfig.salatApiUrl = 'http://api.aladhan.com/v1/timings/';
+salatConfig.salatApiUrl = 'https://api.aladhan.com/v1/timings/';
 
 export default salatConfig;
